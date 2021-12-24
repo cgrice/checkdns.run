bf089138c94f2bde4be674fcfbd688ff2a54f92c
Chris Grice <chris.grice@agentsoftware.net>
2021-12-24 00:42:11 +0000

    Support for loads more record types

--- dns/dns.go
+++ dns/dns.go
@@ -10,7 +10,6 @@ type Record struct {
 	Query      string
 	Result     string
 	TTL        time.Duration
-	Nameserver string
 	RecordType string
 	RawAnswer  string
 }
@@ -19,6 +18,11 @@ func getRecordType(recordtype string) uint16 {
 	lookup := map[string]uint16{
 		"A":     dns.TypeA,
 		"NS":    dns.TypeNS,
+		"PTR":    dns.TypePTR,
+		"MX":    dns.TypeMX,
+		"CERT":    dns.TypeCERT,
+		"SRV":    dns.TypeSRV,
+		"AAAA":    dns.TypeAAAA,
 		"CNAME": dns.TypeCNAME,
 		"TXT":   dns.TypeTXT,
 		"SOA":   dns.TypeSOA,
@@ -44,6 +48,97 @@ func runQuery(message *dns.Msg, nameserver string) (*dns.Msg, time.Duration) {
 	return result, rtt
 }
 
+func transformRecord(answer dns.RR) Record {
+
+	ttl := time.Duration(int64(answer.Header().Ttl) * 1000 * 1000 * 1000)
+	raw := answer.String()
+
+	switch t := answer.(type) {
+	case *dns.A:
+		return Record{
+			Query:      t.Hdr.Name,
+			Result:     t.A.String(),
+			TTL:        ttl,
+			RecordType: "A",
+			RawAnswer:  raw,
+		}
+	case *dns.AAAA:
+		return Record{
+			Query:      t.Hdr.Name,
+			Result:     t.AAAA.String(),
+			TTL:        ttl,
+			RecordType: "AAAA",
+			RawAnswer:  raw,
+		}
+	case *dns.PTR:
+		return Record{
+			Query:      t.Hdr.Name,
+			Result:     t.Ptr,
+			TTL:        ttl,
+			RecordType: "PTR",
+			RawAnswer:  raw,
+		}
+	case *dns.MX:
+		return Record{
+			Query:      t.Hdr.Name,
+			Result:     t.Mx,
+			TTL:        ttl,
+			RecordType: "MX",
+			RawAnswer:  raw,
+		}
+	case *dns.NS:
+		return Record{
+			Query:      t.Hdr.Name,
+			Result:     t.Ns,
+			TTL:        ttl,
+			RecordType: "NS",
+			RawAnswer:  raw,
+		}
+	case *dns.SRV:
+		return Record{
+			Query:      t.Hdr.Name,
+			Result:     t.Target,
+			TTL:        ttl,
+			RecordType: "SRC",
+			RawAnswer:  raw,
+		}
+	case *dns.CERT:
+		return Record{
+			Query:      t.Hdr.Name,
+			Result:     t.Certificate,
+			TTL:        ttl,
+			RecordType: "CERT",
+			RawAnswer:  raw,
+		}
+	case *dns.CNAME:
+		return Record{
+			Query:      t.Hdr.Name,
+			Result:     t.Target,
+			TTL:        ttl,
+			RecordType: "CNAME",
+			RawAnswer:  raw,
+		}
+	case *dns.TXT:
+		return Record{
+			Query:      t.Hdr.Name,
+			Result:     strings.Join(t.Txt, " "),
+			TTL:        ttl,
+			RecordType: "TXT",
+			RawAnswer:  raw,
+		}
+	case *dns.SOA:
+		return Record{
+			Query:      t.Hdr.Name,
+			Result:     t.Ns + " " + t.Mbox,
+			TTL:        ttl,
+			RecordType: "SOA",
+			RawAnswer:  raw,
+		}
+	}
+
+	return Record{}
+}
+
 func Query(domain string, recordtype string, nameserver string) ([]Record, time.Duration) {
 	query := createQuery(domain, recordtype)
 	result, latency := runQuery(query, nameserver)
@@ -51,47 +146,7 @@ func Query(domain string, recordtype string, nameserver string) ([]Record, time.
 	results := make([]Record, len(result.Answer))
 
 	for i, answer := range result.Answer {
-		ttl := time.Duration(int64(answer.Header().Ttl) * 1000 * 1000 * 1000)
-		raw := answer.String()
-		switch t := answer.(type) {
-		case *dns.A:
-			results[i] = Record{
-				Query:      t.Hdr.Name,
-				Result:     t.A.String(),
-				TTL:        ttl,
-				Nameserver: nameserver,
-				RecordType: "A",
-				RawAnswer:  raw,
-			}
-		case *dns.CNAME:
-			results[i] = Record{
-				Query:      t.Hdr.Name,
-				Result:     t.Target,
-				TTL:        ttl,
-				Nameserver: nameserver,
-				RecordType: "CNAME",
-				RawAnswer:  raw,
-			}
-		case *dns.TXT:
-			results[i] = Record{
-				Query:      t.Hdr.Name,
-				Result:     strings.Join(t.Txt, " "),
-				TTL:        ttl,
-				Nameserver: nameserver,
-				RecordType: "TXT",
-				RawAnswer:  raw,
-			}
-		case *dns.SOA:
-			results[i] = Record{
-				Query:      t.Hdr.Name,
-				Result:     t.Ns + " " + t.Mbox,
-				TTL:        ttl,
-				Nameserver: nameserver,
-				RecordType: "SOA",
-				RawAnswer:  raw,
-			}
-		}
-
+		results[i] = transformRecord(answer)
 	}
 
 	return results, latency
